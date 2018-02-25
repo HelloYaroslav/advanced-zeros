cd8679b5b597181f2cde149708c95ee3f2eb0df0
HelloYaroslav <yaroslav.klimenkovv@gmail.com>
2018-02-25 15:15:02 +0300

    'wd'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    // your implementation
+    var NumOfzero = number;
+    var cloneofBase = base;
+    var pow = 0;
+    var lol = 0;
+    var k = 0;
+
+
+
+
+    for (var i = 2; i <= base; i++) {
+        if (cloneofBase % i === 0) {
+            pow = 0;
+            while (cloneofBase % i === 0) {
+                pow++
+                cloneofBase = ~~(cloneofBase / i)
+            }
+            lol = 0
+            k = number
+            while (k / i > 0) {
+                lol = lol + ~~(k / i)
+                k = ~~(k / i)
+            }
+            NumOfzero = Math.min(NumOfzero, (lol / pow))
+        }
+    }
+    console.log(NumOfzero);
+    return NumOfzero
 }
